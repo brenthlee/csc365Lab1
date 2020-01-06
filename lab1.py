0114bfb8837bfea3eef2e9aa711370b8bf0d2d72
system_user <henryluengas@gmail.com>
2020-01-06 11:48:32 -0800

    function requirements

--- lab1.py
+++ lab1.py
@@ -6,3 +6,60 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# S[tudent]: <lastname> [B[us]]
+
+# Search the contents of the students.txt file for the entry (or entries) for students with
+# the given last name.
+
+# For each entry found, print the last name, first name, grade and classroom assignment for
+# each student found and the name of their teacher (last and first name).
+# ###########################################################################################
+
+# T[eacher]: <lastname>
+
+# Search the contents of the students.txt file for the entry (or entries) for students with
+# the given last name.
+
+# For each entry found, print the last name, first name and the bus route the student takes.
+# ###########################################################################################
+
+# B[us]: <number>
+
+# Search the contents of the students.txt file for the entries where the bus route number
+# matches the number provided in the instruction.
+
+# For each such entry, output the first and the last name of the student and their grade and
+# classroom.
+# ###########################################################################################
+
+# G[rade]: <number> [H[igh]|L[ow]]
+
+# Search the contents of the students.txt file for the entries where the student’s grade
+# matches the number provided in the instruction.
+
+# For each entry, output the name (last and first) of the student.
+# ###########################################################################################
+
+# A[verage]: <number>
+
+# Search the contents of the students.txt file for the entries where the student’s grade
+# matches the number provided in the instruction.
+
+# Compute the average GPA score for the entries found. Output the grade level (the number
+# provided in command) and the average GPA score computed.
+# ###########################################################################################
+
+# I[nfo]
+
+# For each grade (from 0 to 6) compute the total number of students in that grade.
+
+# Report the number of students in each grade in the format
+# <Grade>: <Number of Students>
+# sorted in ascending order by grade.
+# ###########################################################################################
+
+# # Q[uit]
+# ###########################################################################################
